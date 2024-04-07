6154029325335851466ccdc083ac34a8aeed0957
Karan Bhardwaj <91610499+karan00034@users.noreply.github.com>
2024-04-07 14:07:16 +0530

    Update FriendListWidget.jsx

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     try {
-      const response = await fetch(`http://localhost:3001/users/${userId}/friends`, {
+      const response = await fetch(`https://mern-social-5hh6.vercel.app/users/${userId}/friends`, {
         method: 'GET',
         headers: { Authorization: `Bearer ${token}` },
       });
