e573850d9cb076e627de4ac4ac3015bdf2794ade
Karan Bhardwaj <91610499+karan00034@users.noreply.github.com>
2024-04-07 14:10:00 +0530

    Update UserWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -23,7 +23,7 @@ import {
     const main = palette.neutral.main;
   
     const getUser = async () => {
-      const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      const response = await fetch(`https://mern-social-5hh6.vercel.app/users/${userId}`, {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
       });
